9141219baa54287ca0b92ea7003498a8bf387bae
David Poindexter <drpoindexter@gmail.com>
2020-09-03 18:21:02 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name;
+  import Buttons from "./Buttons.svelte";
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<hr />
+<Buttons />
---
+++ src/Buttons.svelte
@@ -0,0 +1,29 @@
+<script>
+  let count = 0;
+  let clicks = 0;
+  $: doubled = count * 2;
+  function handleIncrement() {
+    count++;
+    clicks++;
+  }
+  function handleDecrement() {
+    count--;
+    clicks++;
+  }
+</script>
+
+<style>
+  p {
+    color: red;
+  }
+</style>
+
+<button on:click={handleIncrement}> + </button>
+<span>{doubled}</span>
+<span>{count == 0 ? 'Time' : 'Times'}</span>
+<button on:click={handleDecrement}> - </button>
+<hr />
+<p>
+  <span>You have clicked Shiny things {clicks}
+    {clicks == 1 ? 'Time' : 'Times'}</span>
+</p>
